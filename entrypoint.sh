7000f3943476c63725257f9dcf54721bef4536e7
d-rede <diogo.v.rede@gmail.com>
2023-06-26 19:47:02 +0100

    Updated action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,25 @@
 #!/bin/sh -l
 
-ls
-echo "Hello $1"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+echo "args count: $#"
+echo "args list: $@"
+
+
+TRCLI_PARAMS=""
+
+function append_param {
+    local param=$1
+    local value=$2
+    if [ ! -z "$value" ]; then TRCLI_PARAMS="$TRCLI_PARAMS $param $value"; fi
+}
+
+TRCLI_PARAMS=$INSTANCE_PARAMS
+append_param -h $1
+append_param -u $2
+append_param -p $3
+TRCLI_PARAMS="$TRCLI_PARAMS parse_junit"
+append_param -f $4
+
+echo "Result: $TRCLI_PARAMS"
+
+pip install trcli
+trcli --help
